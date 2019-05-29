195134e1bb47d49efa973838b15f6fbb7a91dff7
RamjiB <ramji.b28@gmail.com>
2019-05-29 11:06:38 +0530

    Day 2

---
+++ Image Basics with OpenCV/.ipynb_checkpoints/puppy-checkpoint.py
@@ -0,0 +1,11 @@
+import cv2
+img = cv2.imread('../Data/00-puppy.jpg')
+
+while True:
+    
+    cv2.imshow('Puppy',img)
+    # if we have waited for 1 ms AND we have pressed the Esc key
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
