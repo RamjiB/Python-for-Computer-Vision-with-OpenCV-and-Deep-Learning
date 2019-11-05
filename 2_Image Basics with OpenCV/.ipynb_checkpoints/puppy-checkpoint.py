81a009b9c3756c75b8d5193c8afc7f2c898c2a64
Ramji Balasubramanian <ramji.b28@gmail.com>
2019-11-05 10:43:32 +0530

    Update puppy-checkpoint.py

--- 2_Image Basics with OpenCV/.ipynb_checkpoints/puppy-checkpoint.py
+++ 2_Image Basics with OpenCV/.ipynb_checkpoints/puppy-checkpoint.py
@@ -2,7 +2,6 @@ import cv2
 img = cv2.imread('../Data/00-puppy.jpg')
 
 while True:
-    
     cv2.imshow('Puppy',img)
     # if we have waited for 1 ms AND we have pressed the Esc key
     if cv2.waitKey(1) & 0xFF == 27:
